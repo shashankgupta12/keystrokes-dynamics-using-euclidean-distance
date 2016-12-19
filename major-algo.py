e571bb74ab54f18ab5131032514c52e9afb14953
Shashank Gupta <shashankgupta1994@gmail.com>
2016-12-19 17:48:04 +0530

    completed profile generation and dissimilarity calculation

--- major-algo.py
+++ major-algo.py
@@ -31,72 +31,33 @@ def databaseConnect():
 					dic1["dwell"] = statistics.mean(dwellTime)
 					dic1["latency"] = statistics.mean(latencyTime)
 					masterprofile.append(dic1)
-					dic2["keyCombination"] = "{0}{1}".format(keys[index],keys[index+1])
+					# dic2["keyCombination"] = "{0}{1}".format(keys[index],keys[index+1])
 					dic2["dwell"] = dwellTime
 					dic2["latency"] = latencyTime
 					userprofile.append(dic2)
 
-				for index in range(len(keys) - 1):
-					dissmilarity_list = []	
-					masterDwell = masterprofile[index]["dwell"]
-					masterLatency = masterprofile[index]["latency"]
-					a = numpy.array(masterDwell,masterLatency)
-					for i in range(len(userprofile[index]["dwell"])):
+				dissimilarity = []
+				for i in range(len(userprofile[index]["dwell"])):
+					#the above loop keeps in mind that there is only one richa 
+					#type word typed in the training data as in there is only one
+					#word where 'i' follows 'r', 'c' follows 'i', and so on 
+					for index in range(len(keys) - 1):
+						dissimilarity_list = []	
+						masterDwell = masterprofile[index]["dwell"]
+						masterLatency = masterprofile[index]["latency"]
+						a = numpy.array(masterDwell,masterLatency)
 						dw = userprofile[index]["dwell"][i]
 						la = userprofile[index]["latency"][i]
 						b = numpy.array(dw,la)
 						dist = numpy.linalg.norm(a - b)
-						dissmilarity_list.append(dist)
-					sum(dissmilarity_list)
+						dissimilarity_list.append(dist)
+					dissimilarity.append(sum(dissimilarity_list))
 
-
-
-					documents = collection.find({"sourceKey":keys[index],"targetKey":keys[index+1]})
-					dissmilarity_list = []
-					for docuument in documents:
-						# key = "{0}{1}".format(keys[index],keys[index+1])
-
-					dissmilarity = sum(dissmilarity_list)
-
-
-
-	
-
+				stdev_dissimilarity = statistics.stdev()
+				mean_dissimilarity = statistics.mean()
+				SIGMA = 3.00
+				comparator = mean_dissimilarity + (SIGMA * stdev_dissimilarity)
 
 	except pymongo.errors.ConnectionFailure, e:
 		error_text = "Error: {0}".format(e)
 		print(error_text)
-
-
-
-
-
-
-for json_file_name in open("filelist.txt"):
-	with open(json_file_name) as json_data:
-		data = json.load(data_file)
-		# append data to each of the list above for 20 users
-
-
-
-
-import json
-from pprint import pprint
-# import statistics
-
-with open('data.json') as data_file:    
-    data = json.load(data_file)
-
-# pprint(data)
-
-print(data)
-
-# for time in data:
-# 	key_hold_time.append(time/1000)
-
-# for time in data:
-# 	interkey_time.append(time/1000)
-
-# statistics.stdev()
-# statistics.mean()
-# let selected features be X and Y
