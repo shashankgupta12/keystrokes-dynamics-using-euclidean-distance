7e7112dea037e80ed913902892b6f9a1484523e0
Shashank Gupta <shashankgupta1994@gmail.com>
2017-03-20 03:39:43 +0530

    dataCollect finalized

--- dataCollect.py
+++ dataCollect.py
@@ -1,32 +1,42 @@
 from pynput.keyboard import Key, Listener
 from datetime import datetime
 
-dataentry = []
+keyPressData = []
+keyReleaseData = []
 dataset = []
 
 def on_press(key):
     t = datetime.now()
-    time = t.minute*60*(10**6) + t.second*(10**6) + t.microsecond
-    dataentry.append("p-{0}-{1}".format(key,time))
-    if key == Key.enter:
-        return False
+    # print("p-{0}".format(key))
+    if not key == Key.enter:
+        time = t.minute*60*(10**6) + t.second*(10**6) + t.microsecond
+        keyPressData.append("p-{0}-{1}".format(key,time))
+    # if key == Key.enter:
+    #     return False
 
 def on_release(key):
     t = datetime.now()
-    time = t.minute*60*(10**6) + t.second*(10**6) + t.microsecond
-    dataentry.append("r-{0}-{1}".format(key,time))
+    # print("r-{0}".format(key))
     if key == Key.enter:
         return False
+    else:
+        time = t.minute*60*(10**6) + t.second*(10**6) + t.microsecond
+        keyReleaseData.append("r-{0}-{1}".format(key,time))
 
-# text = str(input("Enter training text: "))
-# print("Enter '{0}' 10 times:".format(text))
-for entry in range(10):
-    # print("Dataset_{0}".format(entry + 1), end='')
-    print('start')
-    with Listener(on_press=on_press, on_release=on_release) as listener:
-        listener.join()
-    dataset.append(dataentry[:-1])
-    dataentry = []
+def dataCollect():
+    global keyPressData
+    global keyReleaseData
+    # text = input("Enter training text: ")
+    print("Enter name 10 times:")
+    for entry in range(10):
+        print("Dataset_{0}".format(entry + 1))
+        # print('start')
+        with Listener(on_press=on_press, on_release=on_release) as listener:
+            listener.join()
+        dataset.append((keyPressData, keyReleaseData))
+        keyPressData = []
+        keyReleaseData = []
 
+dataCollect()
 for dataentry in dataset:
     print(dataentry)
